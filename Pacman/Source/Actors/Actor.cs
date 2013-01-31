9ff344351f770358b4cf3ab2e4eacf1a30bd7852
Niklas Person <valekhz@gmail.com>
2013-02-01 00:19:19 +0100

    Stub code in Level and a Direction enum + Direction for Actors.

--- Pacman/Source/Actors/Actor.cs
+++ Pacman/Source/Actors/Actor.cs
@@ -3,9 +3,19 @@ using SharpDX.Toolkit.Graphics;
 
 namespace Pacman.Actors
 {
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
     public class Actor : Sprite
     {
         protected Vector2 Velocity { get; set; }
+
+        public Direction Direction { get; protected set; }
 
         public Actor(Texture2D texture, Vector2 position, Rectangle sourceRect)
             : base(texture, position, sourceRect)
--- Pacman/Source/Level.cs
+++ Pacman/Source/Level.cs
@@ -1,6 +1,35 @@
 ﻿namespace Pacman
 {
-    class Level
+    public enum TileType
     {
+        Empty,
+        Dot,
+        Energizer,
+        Cherries, // level 1
+        Strawberry, // level 2
+        Peach, // level 3-4
+        Apple, // level 5-6
+        Grapes, // level 7-8
+        Galaxian, // level 9-10
+        Bell, // level 11-12
+        Key, // level 13+
+    }
+
+    public class Level
+    {
+        public static readonly int TilesWide = 28;
+        public static readonly int TilesHigh = 36;
+
+        /// <summary>
+        /// Array listing legal tiles that will be used by the pathfinding.
+        /// 1 means tile is legal to move onto, 0 means illegal.
+        /// </summary>
+        private static readonly int[,] LegalTiles = new int[TilesWide, TilesHigh];
+
+        /// <summary>
+        /// Array storing the state for the current level. Use the TileType
+        /// enum for IDs.
+        /// </summary>
+        private readonly int[,] _tiles = new int[TilesWide, TilesHigh];
     }
 }
